1b55a6fe19e9930f199acbd89d435694d5faadc6
Ntare1 <isaacntare1@gmail.com>
2022-10-17 15:16:21 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for  (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+	if (x < 9)
+		putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
