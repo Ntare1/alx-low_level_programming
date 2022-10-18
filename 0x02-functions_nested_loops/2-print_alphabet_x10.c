000721e92877f2fdc2e538e0267ee4aa53b21f3c
Ntare1 <isaacntare1@gmail.com>
2022-10-18 12:55:09 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point to print 10x
+ */
+void print_alphabet_x10(void)
+{
+	int letters;
+	char y;
+
+	while (letters++ <= 9)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+	}
+	_putchar('\n');
+}
