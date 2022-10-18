471ce7821e2e14d91a64771c42464687444cd4e0
Ntare1 <isaacntare1@gmail.com>
2022-10-18 16:34:40 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit- print last digit
+ * @r: number to be computed
+ * Return: last digit of r
+ */
+int print_last_digit(int r)
+{
+	return ((r % 10) + '0');
+}
