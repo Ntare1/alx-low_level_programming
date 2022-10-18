3155d01165fffd8c2751a47e1a45f1d62e0a2e6c
Ntare1 <isaacntare1@gmail.com>
2022-10-18 16:24:01 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs- shows absolute of integer
+ * @r: integer to be checked
+ * Return: absolute value of integer
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}
