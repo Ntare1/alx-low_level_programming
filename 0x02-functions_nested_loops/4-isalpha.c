712f3616c6c0d3cf225a002852b21957469de4ef
Ntare1 <isaacntare1@gmail.com>
2022-10-18 15:16:29 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks character is alphabet
+ * @c: character to be checked
+ * Return: 1 in success
+ * otherwise return 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+			return (1);
+	}
+	else
+		return (0);
+}
