c99da3cc9e6b72967e3822a9ce70eeaebbe2875d
Ntare1 <isaacntare1@gmail.com>
2022-10-19 11:17:01 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_baeur- the 24 hour clock
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 24; x++)
+	{
+		for (y = 0; y <= 60; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+
+}
