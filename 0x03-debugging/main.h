bcfb39bd335d3df47a4314e7cd2e7262e146c06d
Ntare1 <isaacntare1@gmail.com>
2022-10-20 12:42:54 -0700

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+/**
+ * _putchar - our function
+ * @c: character
+ * Return: always character
+ */
 void positive_or_negative(int i);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int r);
+int print_last_digit(int r);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
 #endif
