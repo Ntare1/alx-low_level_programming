ffa5542dacf3484ae46cfbca0702e6ec1a02e378
Ntare1 <isaacntare1@gmail.com>
2022-10-20 13:12:52 -0700

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+/**
+ * _putchar - our function
+ * @c: character
+ * Return: always character
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int r);
+int print_last_digit(int r);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
 #endif
