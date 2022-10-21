ee2ad81f5f17e9305b41422f609e8895f42da600
Ntare1 <isaacntare1@gmail.com>
2022-10-21 08:35:17 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
  * @c: character to be checked
  * Return: 1 if character is upeper or else return 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
