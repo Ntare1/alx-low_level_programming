950ac2118315feb8ee6f40f20371c452d1ad4bbf
Ntare1 <isaacntare1@gmail.com>
2022-10-21 18:56:31 -0700

    task2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,11 @@
 
 /**
  * mul- multiplies different integers
+ * @a: to be multipliied
+ * @b: to be multiplied
  * Return: 0
  */
 int mul(int a, int b)
 {
-	putchar(a * b);
-	return (0);
+	return (a * b);
 }
