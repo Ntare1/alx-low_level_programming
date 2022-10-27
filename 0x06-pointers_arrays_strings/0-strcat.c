604b06b0a5e1d0dd54c6e11d24c37eebc4efd44a
Ntare1 <isaacntare1@gmail.com>
2022-10-27 13:19:20 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@ char *_strcat(char *dest, char *src)
 	int y;
 
 	for (x = 0; dest[x] != '\0'; x++)
-		for (y = 0; src[y] != '\0'; )
+		for (y = 0; src[y] != '\0'; y++)
 		{
 			dest[x] = src[y];
 			x++;
 			y++;
 		}
-	dest[x + y] = '\0';
+	dest[x] = '\0';
 	return (dest);
 }
