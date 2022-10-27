24948731b8fe4bc8ba8cbe74197264a73cf35608
Ntare1 <isaacntare1@gmail.com>
2022-10-27 14:47:12 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int x = 0;
 	
-	if (*s1 != '\0' || *s2 != '\0')
-		return (1);
-	if (x == 0)
-		return (0);
-	else
-		return (1);
+	while (*s1[x] != '\0' && *s2[x] != '\0')
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		x++;
+	}
+	return (0);
 }
