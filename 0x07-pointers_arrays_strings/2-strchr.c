3cde442e382ae19768eb868ec88b91a58d7bb8d5
Ntare1 <isaacntare1@gmail.com>
2022-10-31 13:58:32 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to be searched
+ * @c: The character to be located
+ * Return: If c is found or else retun NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (x = 0; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (s + x);
+	}
+	return ('\0');
+}
