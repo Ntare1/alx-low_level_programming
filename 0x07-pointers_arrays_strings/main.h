d03a9a4414b3813b4196be2269151e0184d3ef8d
Ntare1 <isaacntare1@gmail.com>
2022-10-31 13:03:10 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+char *_strcat(char *dest, char *src);
+#endif /* MAIN.H */
