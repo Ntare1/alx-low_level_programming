7b49e9e62fc485a635ecb2f798cf38dc5077e8bc
Ntare1 <isaacntare1@gmail.com>
2022-11-06 12:38:31 -0800

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * @argc: input argument
+ * @argv: array of input argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
