871bd2f088ec854945e41c3677f0fe45c0f80df5
Ntare1 <isaacntare1@gmail.com>
2022-11-17 13:19:07 -0800

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 	const unsigned int i;
 	int sum = 0;
 
-	va_start(nt, n);
 	if (n == 0)
-	{
 		return (0);
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-			sum += va_arg(const unsigned int, nt);
-	}
+
+	va_start(nt, n);
+
+	for (i = 0; i < n; i++)
+			sum += va_arg(nt, int);
+
 	va_end(nt);
+
 	return (sum);
 }
