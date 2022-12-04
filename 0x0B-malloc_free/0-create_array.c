dd3e92eaa9d160439edb359de8d7dad43734964c
Ntare1 <isaacntare1@gmail.com>
2022-12-04 04:38:57 -0800

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,17 +9,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    unsigned int y;
-    char *x;
+	unsigned int y;
+	char *x;
 
-    if (size == 0)
-    retutn (NULL)
-    x = malloc(size * sizeof(char))
-    if (x == 0)
-    return (NULL)
-    for (y = 0; y < size; y++)
-    {
-        x[y] = c;
-    }
-    return (x);
-}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	x = malloc(size * sizeof(char);
+			if (x == 0)
+			{
+			return (NULL);
+			}
+			for (y = 0; y < size; y++)
+			{
+			x[y] = c;
+			}
+			return (size);
+			}
