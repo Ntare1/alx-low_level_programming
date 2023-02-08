a1685d5ee36e2c960177610409d9035e00f6886e
Ntare1 <isaacntare1@gmail.com>
2023-02-08 09:39:36 -0800

    control statements

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+				putchar((z % 10) + '0');
+				if (x == 8 && y == 9 && z == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
