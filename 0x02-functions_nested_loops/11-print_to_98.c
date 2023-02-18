83818d4db56b17ee3e82dfe8e7a884511a989e64
Ntare1 <isaacntare1@gmail.com>
2023-02-17 22:33:59 -0800

    function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,23 +7,21 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			printf("%d", i);
-			if (i != 98)
+			printf("%d", n);
+			if (n != 98)
 				printf(", ");
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (; n >= 98; n--)
 		{
-			printf("%d", i);
-			if (i != 98)
+			printf("%d", n);
+			if (n != 98)
 				printf(", ");
 		}
 	}
