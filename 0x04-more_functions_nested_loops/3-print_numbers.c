c8f3f659b4c87b5a1f95b40c51e9d162239936ae
Ntare1 <isaacntare1@gmail.com>
2023-02-21 09:05:21 -0800

    print numbers excluding 2&4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,11 @@ void print_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if ((x == 2) || ((x == 4)
-					continue;
-					_putchar(x);
+		if ((x == 2) || (x == 4))
+			continue;
+		_putchar(x + '0');
 	}
-	_putchar("\n");
+	_putchar('\n');
 
 }
 
