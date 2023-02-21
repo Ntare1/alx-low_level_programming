0a070bcf5b6900eda11886b750ab480b9dfbccdb
Ntare1 <isaacntare1@gmail.com>
2023-02-21 13:13:06 -0800

    printing line diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,9 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-	for (x = 0; x < n; x++)
+	for (x = 1; x <= n; x++)
 	{
-		for (y = 0; y < x; y++)
+		for (y = 1; y < x; y++)
 		{
 			_putchar(' ');
 		}
