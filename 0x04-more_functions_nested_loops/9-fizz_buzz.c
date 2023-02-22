7d021d14ae35c012cf62a04d6746c96fc37bf6cc
Ntare1 <isaacntare1@gmail.com>
2023-02-22 15:32:13 +0200

    Fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main- entry to print to 100
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		printf("\n");
+	}
+}
