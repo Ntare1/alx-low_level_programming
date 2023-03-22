5a97a233610d71a7aa6b35b47ed55bd014562ab1
Ntare1 <isaacntare1@gmail.com>
2023-03-22 21:50:23 +0200

    Dynamic library

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar(95);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _atoi - convert string to integer
+ *
+ * @s: the pointer to convert
+ * Return: 0
+ */
+ int _atoi(char *s)
