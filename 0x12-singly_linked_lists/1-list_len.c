9a26d4780627353c3fe9dedb8676dad73dbc5c18
Ntare1 <isaacntare1@gmail.com>
2023-04-08 13:29:01 -0700

    SIngly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,12 +8,12 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        num++;
-    }
-    return(num);
+	while (h != NULL)
+	{
+		h = h->next;
+		num++;
+	}
+	return (num);
 }
