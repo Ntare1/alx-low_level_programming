2e57ae5280d89580a482086917f8161ceb7634d4
Ntare1 <isaacntare1@gmail.com>
2023-04-09 12:04:04 -0700

    Linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (head != NULL && str != NULL)
 	{
-		inter = malloc(sizeof(list_t);
+		inter = malloc(sizeof(list_t));
 				if (inter == NULL)
 				return (NULL);
 
