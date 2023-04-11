bb78ee233041c541f80b1f718b17c5630d4d1d2e
Ntare1 <isaacntare1@gmail.com>
2023-04-11 14:14:37 -0700

    Attributes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * bef_main-prints the string before main is executed
+ */
+void bef_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
