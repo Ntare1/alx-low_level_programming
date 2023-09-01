c59331475853215c3a6c7d11ae666af463599f4e
Ntare1 <isaacntare1@gmail.com>
2023-09-01 21:55:16 +0200

    Static lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Entry point
+ * @str: string
+ */
+void _puts(char *str)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+}
