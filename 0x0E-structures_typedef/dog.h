f78cbfd6882f5c837e8acb9e8815ac6e3df30095
Ntare1 <isaacntare1@gmail.com>
2023-09-17 09:58:57 -0700

    Structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog- a structure that details a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog 's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
