a3318ab2d5f22914ab96848f56e98f3ca0ef7e48
Ntare1 <isaacntare1@gmail.com>
2023-09-25 10:19:43 +0200

    Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int x, 1;
+    
+    if (argc != 2) {
+        printf("Error\n");
+        return (1);
+    }
+
+    x = atoi(argv[1]);
+
+    if (x < 0) {
+        printf("Error\n");
+        return (2);
+    }
+
+    void *mainx = main;
+
+    for (int i = 0; i < x; i++) {
+        unsigned char opcode;
+        int result = *((unsigned char *)(mainx + i));
+
+        printf("%02x", result);
+
+        if (i < x - 1) {
+            printf(" ");
+        } else {
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
