aa5dcc6d4ef63b29d05c8d85cbd099ae62d88f46
Ntare1 <isaacntare1@gmail.com>
2023-09-26 14:46:46 -0700

    linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,8 +13,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *node;
 
 	node = (listint_t *)malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
 	node->n = n;
 	node->next = *head;
+
 	*head = node;
+
 	return (node);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,7 +10,7 @@ void free_listint(listint_t *head)
 {
 	listint_t *node;
 
-	if (head != NULL)
+	if (head)
 	{
 		node = head->next;
 		free(head);
