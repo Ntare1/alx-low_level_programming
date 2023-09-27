5aed5f0fe3170eb6832c8497a84087125550f81a
Ntare1 <isaacntare1@gmail.com>
2023-09-27 14:01:23 -0700

    Malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,45 +1,49 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
-* _realloc - reallocates a memory block
-* @ptr: pointer to the memory previously allocated with a call to malloc
-* @old_size: size of ptr
-* @new_size: size of the new memory to be allocated
-*
-* Return: pointer to the address of the new memory block
-*/
-
+ * _realloc - function that reallocates a memory block
+ * @ptr: pointer
+ * @old_size: Old
+ * @new_size: New
+ *
+ * Return: Success
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
+	char *nptr;
 	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
 
 	if (ptr == NULL)
 	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
-		return (ptr);
+		nptr = malloc(new_size);
 
-	else if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
 	}
 	else
 	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
+		if (new_size == 0)
 		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
 			free(ptr);
-			return (temp_block);
-		}
-		else
 			return (NULL);
+		}
 	}
+	nptr =  malloc(new_size);
+
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
+	free(ptr);
+
+	return (nptr);
 }
