6e7e6b019b29082e8e0651c7342da613ea1b3f99
Ntare1 <isaacntare1@gmail.com>
2024-01-04 14:21:51 -0800

    adding line to check if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,11 @@ int create_file(const char *filename, char *text_content)
 	if (ptr == -1)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
+
 	while (text_content[x] != '\0')
 	{
 		x++;
